622d4543b2c9e93f418f78bcdb21b53eccda0436
Lewis Reid <lreidwilliams@gmail.com>
2024-11-07 21:10:11 +1000

    increased size of room and centered it in the window

--- game.rb
+++ game.rb
@@ -8,7 +8,7 @@ require_relative 'lib/graphics'
 tile_size = 64
 Position.scale = tile_size
 room_min_pos = Position.at(tile_size, tile_size)
-room_bounds = Bounds.new(room_min_pos, room_min_pos.step(5, 5))
+room_bounds = Bounds.new(room_min_pos, room_min_pos.step(11, 7))
 room = Room.new(room_bounds, &Graphics.method(:tile))
 
 keyboard_driver = Drivers::Keyboard.new
@@ -26,5 +26,5 @@ update do
   room.update
 end
 
-set title: 'QUEST', borderless: true
+set title: 'QUEST', borderless: true, width: 14 * tile_size, height: 10 * tile_size
 show
